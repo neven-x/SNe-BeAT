62431cd5278820d7b692cfa4fae0fbf4eabaa2fd
borakn <neven.borak@crick.ac.uk>
2023-11-14 15:49:54 +0000

    Established initial package structure.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='SNe-BeAT',
-    author='Neven Borak',
+    author='Neven Borak, nevenborak@icloud.com',
     version='0.0.1',
     description='Tools for analysis of synchronously acquired neural and behavioural data.',
     packages=find_packages(),
